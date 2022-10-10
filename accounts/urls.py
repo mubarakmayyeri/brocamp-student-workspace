96aba488645464dfcb173e8a582bacfd5edc9f6e
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2022-10-10 06:13:50 +0530

    set up session handling

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signin', views.signin, name='signin'),
     path('signup', views.signup, name='signup'),
-    path('signout', views.signin, name='signout'),
+    path('signout', views.signout, name='signout'),
+    path('workspace', views.workspace, name='workspace'),
+    path('tasks', views.tasks, name='tasks'),
+    path('profile', views.profile, name='profile'),
+    path('contact', views.contact, name='contact')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,15 @@
 from urllib import request
 from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
+  if 'username' in request.session:
+    return render(request, 'workspace.html')
   return render(request, 'home.html')
 
 def signup(request):
@@ -21,6 +24,9 @@ def signup(request):
   return render(request, 'signup.html')
 
 def signin(request):
+  if 'username' in request.session:
+    return redirect('workspace')
+ 
   if request.method == 'POST':
     username = request.POST['username']
     pass1 = request.POST['pass1']
@@ -28,8 +34,9 @@ def signin(request):
     user = authenticate(username=username, password=pass1)
     
     if user is not None:
+      request.session['username'] = username
       login(request, user)
-      return render(request, 'index.html', {'uname':username})
+      return render(request, 'workspace.html', {'uname':username})
       
     else:
       messages.error(request, "Wrong username/password")
@@ -37,3 +44,28 @@ def signin(request):
     
     
   return render(request, 'signin.html')
+
+def signout(request):
+  if 'username' in request.session:
+    request.session.flush()
+  logout(request)
+  messages.success(request, "Logged out successfully.")
+  return redirect('home')
+
+def workspace(request):
+  if 'username' in request.session:
+    return render(request, 'workspace.html')
+  return redirect(signin)
+
+def tasks(request):
+  if 'username' in request.session:
+    return HttpResponse('<h1>Tasks Page</h1>')
+  return redirect(signin)
+  
+def profile(request):
+  if 'username' in request.session:
+    return HttpResponse('<h1>Student Profile</h1>')
+  return redirect(signin)
+
+def contact(request):
+  return HttpResponse('<h1>Contact Page</h1>')
